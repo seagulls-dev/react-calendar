85ee916467ea73f804497f5241bd952e7abfc401
seagulls-lucky <seagulls.lucky125@gmail.com>
2021-08-12 03:14:07 +0900

    initial

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import DemoApp from "./components/DemoApp";
+import './main.css'
+
+ReactDOM.render(<DemoApp />, document.getElementById('root'));
